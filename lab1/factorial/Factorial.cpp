6d3d00236f850e4176fb59c38e4aa4625c9f879e
pawelgalka <p.k.galka@gmail.com>
2018-03-02 17:37:43 +0100

    lab1

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,11 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+  if (value > 12 || value < -12) return 0;
+  if (value == 0) return 1;
+  else {
+    if (value < 0) return value * factorial(value + 1);
+    else return value * factorial(value - 1);
+  }
+
 }

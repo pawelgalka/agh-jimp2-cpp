8b9259bb149b7261d47fd447f2a5649ca589480c
pawelgalka <p.k.galka@gmail.com>
2018-03-09 16:45:02 +0100

    Refactoring code of factorial

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -3,12 +3,16 @@
 //
 #include "Factorial.h"
 
-int factorial(int value) {
-  if (value > 12 || value < -12) return 0;
+bool IsOutOfRange(int value);
+
+int Factorial(int value) {
+  if (IsOutOfRange(value)) return 0;
   if (value == 0) return 1;
   else {
-    if (value < 0) return value * factorial(value + 1);
-    else return value * factorial(value - 1);
+    if (value < 0) return value * Factorial(value + 1);
+    else return value * Factorial(value - 1);
   }
 
 }
+
+bool IsOutOfRange(int value) { return value >= 13 || value <= -13; }
